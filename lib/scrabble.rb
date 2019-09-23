fbaf2614475ce13e74fe3c2d97293d1fb6f0f69b
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-23 17:41:15 +0100

    Completes scrabble challenge

---
+++ lib/scrabble.rb
@@ -0,0 +1,27 @@
+class Scrabble
+
+  def initialize(word)
+    @scores = {
+      " " => 0,
+      "AEIOULNRST" => 1,
+      "DG" => 2,
+      "BCMP" => 3,
+      "FHVWY" => 4,
+      "K" => 5,
+      "JX" => 8,
+      "QZ" => 10
+    }
+    @word = word
+  end
+
+
+  def score
+    return 0 if !@word.is_a? String
+    score = 0
+    @word.chomp('\t\n').upcase.chars do |letter|
+      @scores.each { |l, s| score += s if l.include? letter }
+    end
+    score
+  end
+
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,49 @@
+require 'scrabble'
+
+
+describe Scrabble do
+
+
+  describe '#score' do
+    it 'returns 1 for the letter a' do
+      scrabble = Scrabble.new("a")
+      expect(scrabble.score).to eq 1
+    end
+
+    it 'returns 4 for the letter f' do
+      scrabble = Scrabble.new("f")
+      expect(scrabble.score).to eq 4
+    end
+
+    it 'returns 6 for the word street' do
+      scrabble = Scrabble.new("street")
+      expect(scrabble.score).to eq 6
+    end
+
+    it 'returns 0 for nil' do
+      scrabble = Scrabble.new(nil)
+      expect(scrabble.score).to eq 0
+    end
+
+    it 'returns 0 for the a space' do
+      scrabble = Scrabble.new(" ")
+      expect(scrabble.score).to eq 0
+    end
+
+    it 'returns 22 for the word quirky' do
+      scrabble = Scrabble.new("quirky")
+      expect(scrabble.score).to eq 22
+    end
+
+    it 'returns 41 for the word OXYPHENBUTAZONE' do
+      scrabble = Scrabble.new("OXYPHENBUTAZONE")
+      expect(scrabble.score).to eq 41
+    end
+
+    it 'returns 0 for " \t\n"' do
+      scrabble = Scrabble.new(' \t\n')
+      expect(scrabble.score).to eq 0
+    end
+
+  end
+end
